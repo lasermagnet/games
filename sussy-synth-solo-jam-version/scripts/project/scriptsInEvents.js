850bb644adb15f5ccfa88f062390c49671d00dc5
fodi <1785582+fodi@users.noreply.github.com>
2024-11-06 16:24:39 +0100

    Added Sussy Synth Solo (jam version)

---
+++ sussy-synth-solo-jam-version/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async ["Futilities🧰_Event8_Act1"](runtime, localVars)
+	{
+		runtime.setReturnValue(runtime.objects[localVars["name"]] ? 1 : 0)
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
